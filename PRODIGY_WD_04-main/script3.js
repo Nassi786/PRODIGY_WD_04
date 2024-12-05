168b4966f0143bf642f04c64a5169a6a305a3264
Nassi786 <syednaseera421@gmail.com>
2024-12-05 20:02:20 +0530

    Add files via upload

---
+++ PRODIGY_WD_04-main/script3.js
@@ -0,0 +1,17 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Smooth scrolling for navigation links
+    document.querySelectorAll('nav a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
+
+
+
+
